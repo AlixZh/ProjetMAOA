2438b85ac05375ec4399bf3f115303928353c12a
AlixZh <72941464+AlixZh@users.noreply.github.com>
2021-11-10 12:17:22 +0100

    Update VRP.jl

--- VRP.jl
+++ VRP.jl
@@ -1,42 +1,44 @@
 using JuMP
 using CPLEX
 
-
+#include("tools.jl")
 const OPTIMAL = JuMP.MathOptInterface.OPTIMAL
 const INFEASIBLE = JuMP.MathOptInterface.INFEASIBLE
 const UNBOUNDED = JuMP.MathOptInterface.DUAL_INFEASIBLE;
 
 
 #le centre/source de dépot a un noeud d'indice 1 
-function PL_VRP(lqi,ldi,data)
+function PL_VRP(data,t)
 	"""
 	Parametres : 
 	lqi : (vecteur), lqi[i] quantité a etre livree au vendeur i, 1<=i<=n
-	lqi[i] : le client i doit recevoir lqi[i] 1<i<=n, 
+	lqi[i] : le client i doit recevoir ldi[i] 1<i<=n, 
 	ldi : les revendeurs qui vont etre livree, ldi[i] : contient le nom du revendeur, 1<=ldi[i]<=n
 	1<=i<=length(ldi)
 	l'indice 1 est bien le client et non le centre de depot dans ldi
-	data : dictionnaire contenant les infos d une instance
+	data : dictionnaire contenant les info d une instance
 	"""
 
 	lci=matrix_cout(data) #l'indice 1 est le centre de depot
 						  # le cout du client i et j dans lci : ldi[i+1] et ldi[j+1]
+						  
+	ldi = data["d"][:,t]
 	# Création d'un modèle. Ce modèle fera l'interface avec le solveur GLPK
 	m = Model(CPLEX.Optimizer)
 	# Création des variables
 	@variable(m, 0<=x[1:data["n"]+1 , 1:data["n"]+1]<=1,Int)
 	@variable(m,0<=w[1:data["n"]]) #indice 1 correspond au client 1
 	@objective(m, Min, sum(x .*lci) )
-
+	
 
 	@constraint(m,sum(x[1,:])<=data["k"]) #indice 1 correspond au depot 0
 	@constraint(m,sum(x[:,1])<=data["k"])
 	for i in [1:length(ldi);]
-		@constraint(m,sum(x[Int(ldi[i]+1),:])==1)
-		@constraint(m,sum(x[:,Int(ldi[i]+1)])==1)
+		@constraint(m,sum(x[Int(i+1),:])==1)
+		@constraint(m,sum(x[:,Int(i+1)])==1)
 		@constraint(m,0<=w[i]<=data["Q"])
 		for j in [1:length(ldi);]
-			@constraint(m, (lqi[Int(ldi[i])]-(data["Q"]+lqi[Int(ldi[i])])*(1-x[i+1,j+1])) <= (w[i]-w[j]))
+			@constraint(m, (Int(ldi[i])-(data["Q"]+Int(ldi[i]))*(1-x[i+1,j+1]))<= (w[i]-w[j]))
 		end
 	end
 
@@ -79,12 +81,10 @@ end
 
 
 
-#FONCTIONNE 
+#FONCTIONNE PAS
 dataA_014_ABS1_15_1=Read_file("./PRP_instances/A_014_ABS1_15_1.prp")
 qq=dataA_014_ABS1_15_1["d"][:,2]
-dd=[1 4 8]
-PL_VRP(qq,dd,dataA_014_ABS1_15_1)
-
+PL_VRP(dataA_014_ABS1_15_1,2)
 
 
 
