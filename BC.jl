482a3e4d343e008bc8010457ca727378534c7ecd
AlixZh <72941464+AlixZh@users.noreply.github.com>
2021-12-13 22:02:40 +0100

    PDI exacte , BC et Tests

---
+++ BC.jl
@@ -0,0 +1,256 @@
+using IterTools
+using JuMP
+using CPLEX
+include("PDI1.jl")
+include("PDI2.jl")
+include("toolsPDI.jl")
+include("tools.jl")
+
+
+#---------------------------------------------------------------
+#PRP 2
+#---------------------------------------------------------------
+function BC2(data)
+	"""
+	data : donnees des instances sous forme de dictionnaire
+	renvoie la solution optimale , utilisation du branch and cut et du PLNE PDI2
+	"""
+
+	#m=PLNE_PDI2(data,true) #model du PLNE PDI2
+	branch=true
+	lci=matrix_cout(data) #l'indice 1 est le centre de depot
+						  # le cout du client i et j dans lci : ldi[i+1] et ldi[j+1]
+
+	# Création d'un modèle. Ce modèle fera l'interface avec le solveur GLPK
+	m = Model(optimizer_with_attributes(CPLEX.Optimizer, "CPX_PARAM_EPINT" => 1e-15 ))
+	# Création des variables
+	@variable(m,y[1:data["l"]],Bin) #(32)
+	@variable(m,z[1:data["n"]+1, 1:data["k"],1:data["l"]],Bin)
+	@variable(m,x[1:data["n"]+1, 1:data["n"]+1, 1:data["k"],1:data["l"]],Bin)
+	@variable(m, 0 <= p[1:data["l"]] ) #(31)
+	@variable(m, 0 <= I[1:data["n"]+1, 1:data["l"]])
+	@variable(m, 0 <= q[1:data["n"]+1, 1:data["k"], 1:data["l"]])
+
+	@objective(m, Min, sum(data["u"]*p + data["f"]*y + sum( data["h"] .*I,dims=1 )[1,:] + sum(sum(lci .*sum(x,dims=3), dims=1), dims=2)[1,1,1,:] )) 
+	#@objective(m, Min, sum(data["u"]*p + data["f"]*y + sum( data["h"] .*I,dims=1 )[1,:] + sum( lci[i,j]*sum(x[i,j,:,t] for k =1:data["k"]) for i =1:data[n+1]+1, for j =1:data["n"]+1 if (i!=j)) )) 
+	
+
+	#contraintes
+	for t = 1:data["l"]
+		if(t>1)
+			@constraint(m, I[1,t-1]+p[t] == sum(q[i+1,k,t] for i=1:data["n"] for k=1:data["k"]) + I[1,t]) #21
+		else
+			@constraint(m, data["L0"][1]+p[t] == sum(q[i+1,k,t] for i=1:data["n"] for k=1:data["k"]) + I[1,t]) #21
+		end
+		@constraint(m,p[t]<=Mt(data,t)*y[t]) #23
+		@constraint(m, I[1,t]<=data["L"][1]) #24
+
+		for i =2: data["n"]+1
+			
+			@constraint(m,sum(z[i,:,t])<=1) #(27)
+			if( t>1)
+				@constraint(m,I[i,t-1]+sum(q[i,:,t]) <= data["L"][i]) #(25)
+				@constraint(m,I[i,t-1]+sum(q[i,:,t])==data["d"][i-1,t]+I[i,t]) #22
+			else
+				@constraint(m,data["L0"][i]+sum(q[i,:,t]) <= data["L"][i]) #(25)
+				@constraint(m,data["L0"][i]+sum(q[i,:,t])==data["d"][i-1,t]+I[i,t]) #22
+			end
+		end
+		for k =1:data["k"]
+			for i =1:data["n"]+1
+				@constraint(m,x[i,i,k,t]==0)
+			end
+			@constraint(m,sum(q[i,k,t] for i=2:data["n"]+1)<= data["Q"]*z[1,k,t]) #(30)
+			@constraint(m,sum(x[1,j,k,t] for j=1:data["n"]+1 if j!=1)+sum(x[j,1,k,t] for j =1:data["n"]+1 if j!=1)==2*z[1,k,t]) #28
+			
+			for myi in [2:data["n"]+1;]
+				@constraint(m,q[myi,k,t]<=Mit_til(data,myi-1,t)*z[myi,k,t]) #(26)
+				@constraint(m,( sum(x[myi,j,k,t] for j =1:data["n"]+1 if j!=myi) + sum(x[j,myi,k,t] for j =1:data["n"]+1 if j!=myi) )== 2*z[myi,k,t]) #28
+				
+				#for s in subsets([2 : data["n"]+1],myi)
+			end
+			if(!branch)
+				for s in powerset([ind for ind=2:data["n"]+1],2,data["n"])
+					@constraint(m, sum(x[i,j,k,t] for i in s for j in s if i!=j) <= length(s)-1) #(29)
+				end
+			end
+		
+		end
+	end
+	
+	function lazySep_Violated(cb_data)
+		for t = 1:data["l"]
+			for k =1:data["k"]
+				xsep=zeros(Int,data["n"]+1,data["n"]+1)
+				for i=1:data["n"]+1
+					for j =1:data["n"]+1
+						if( callback_value(cb_data,x[i,j,k,t]) > 0.9)
+							xsep[i,j]=1
+						end
+					end
+				end
+				println(xsep)
+				departDepot,tournee=detect_sous_tour2(data["n"],xsep)
+				println(departDepot,tournee)
+				
+				if(!departDepot)
+					if(length(tournee)>=2 && !(1 in tournee))
+						for ee in tournee
+							#con=@build_constraint(sum(variable_by_name(m,"x[$i,$j,$k,$t]") for i in tournee for j in tournee if i!=j ) <= sum(variable_by_name(m,"z[$i,$k,$t]") for i in tournee) -variable_by_name(m,"z[$e,$k,$t]"))
+							con=@build_constraint(sum(x[i,j,k,t] for i in tournee for j in tournee if i!=j ) <= sum(z[i-1,k,t] for i in tournee) -z[ee-1,k,t])
+							MOI.submit(m,MOI.LazyConstraint(cb_data),con)
+						end
+					end
+				end
+			end
+			
+		end
+	end
+	
+	
+	MOI.set(m,MOI.LazyConstraintCallback(),lazySep_Violated)
+	
+	optimize!(m)
+	
+	status = termination_status(m)
+	if status == JuMP.MathOptInterface.INFEASIBLE
+		println("Le problème n'est pas réalisable")
+	elseif status == JuMP.MathOptInterface.DUAL_INFEASIBLE
+		println("Le problème est non borné")
+	elseif status == JuMP.MathOptInterface.OPTIMAL
+		xopt,yopt,qopt,popt=get_param_opt2(data,m)
+		println("Temps de résolution :", solve_time(m))
+		println("optimum = ", objective_value(m)) 
+		return true,objective_value(m), solve_time(m) ,xopt,yopt,qopt,popt,relative_gap(m)
+	end
+	
+
+end
+
+
+
+#---------------------------------------------------------------
+#PRP 1
+#---------------------------------------------------------------
+
+function BC1(data)
+	"""
+	data : donnees des instances sous forme de dictionnaire
+	renvoie la solution optimale , utilisation du branch and cut et du PLNE PDI1
+	"""
+	#m=PLNE_PDI1(data,true)
+	branch=true
+	lci=matrix_cout(data) #l'indice 1 est le centre de depot
+						  # le cout du client i et j dans lci : ldi[i+1] et ldi[j+1]
+
+	# Création d'un modèle. Ce modèle fera l'interface avec le solveur GLPK
+	m = Model(optimizer_with_attributes(CPLEX.Optimizer, "CPX_PARAM_EPINT" => 1e-15 ))
+	#set_optimizer_attribute(m,"CPLEX_PARAM_TLIM",
+	# Création des variables
+	@variable(m, 0 <= p[1:data["l"]]) #quantite de production a la periode t
+	@variable(m, y[1:data["l"]],Bin)
+	@variable(m, z[1:data["n"], 1:data["l"]],Bin) # z_it=1 si le revendeur i est visite a la periode t, i in Nc
+	@variable(m, 0 <= z0[1:data["l"]] <= data["k"],Int) #le nombre de vehicule qui part du depot noeud 0 a la periode t
+	@variable(m, 0 <= w[1:data["n"] , 1: data["l"]] )
+	
+	@variable(m,0<=I[1:data["n"]+1,1:data["l"]])#inventaire du noeud i a la fin de la periode t
+	@variable(m,0 <= q[1:data["n"]+1,1:data["l"]],Int) #quantite delivree au revendeur i a la periode tincl
+	@variable(m, x[1:data["n"]+1,1:data["n"]+1,1:data["l"]],Bin) #x_ijt=1 si un vehicule voyage directement du noeud i au noeud j a l instant t
+	
+	@objective(m, Min, sum(data["u"]*p + data["f"]*y + sum( data["h"] .*I,dims=1 )[1,:] + sum(sum(lci .*x, dims=1), dims=2)[1,1,:] )) 
+	
+	#contraintes
+	for t in [1:data["l"];]
+		for i =1:data["n"]+1
+			@constraint(m,x[i,i,t]==0)
+		end
+		if(t==1)
+			@constraint(m,data["L0"][1]+p[t] == sum(q[:,t])-q[1,t]+I[1,t] ) #(2) #t-1==0
+			for i in [2: data["n"]+1;] 
+				@constraint(m,data["L0"][i]+q[i,t] == data["d"][i-1,t] + I[i,t]) #(3) cas t-1==0
+				@constraint(m,data["L0"][i]+q[i,t] <= data["L"][i]) #(6) #cas ou t-1==0
+			end
+		else 
+			@constraint(m,I[1,t-1] + p[t] == sum(q[:, t])-q[1,t]+I[1,t]) #(2) cas t-1>0
+		end
+		@constraint(m,p[t]<=Mt(data,t)*y[t]) #(4)
+		@constraint(m,I[1,t] <= data["L"][1]) #(5)
+		@constraint(m,z0[t]<=data["k"]) #(10) 
+		for i in [1: data["n"]+1;]
+			if(i==1)
+				@constraint(m,sum(x[i,k,t] for k =1:data["n"]+1 if i!=k)+ sum(x[k,i,t] for k =1:data["n"]+1 if k!=i)==2*z0[t]) #(9) cas ou i represente le centre de depot
+				if(!branch)
+					@constraint(m, w[i,t] <= data["Q"]*z0[t]) #(12)
+				end
+			elseif(i>1)
+				if (t>1)
+					@constraint(m,I[i,t-1]+q[i,t] == data["d"][i-1,t] + I[i,t]) #(3) t-1>0,i \in Nc
+					@constraint(m,I[i,t-1]+q[i,t]<=data["L"][i]) #(6) #cas ou t-1>0, i dans Nc
+				end
+				@constraint(m,q[i,t]<=Mit_til(data,i-1,t)*z[i-1,t]) #(7) #i dans Nc, z[i-1,t] represente le revendeur i
+				@constraint(m,sum(x[i,k,t] for k =1:data["n"]+1 if(k!=i))==z[i-1,t]) #(8)
+				@constraint(m,sum(x[i,k,t] for k =1:data["n"]+1 if k!=i) + sum(x[k,i,t] for k =1:data["n"]+1 if k!=i)==2*z[i-1,t]) #(9)
+				if(!branch)
+					@constraint(m, w[i-1,t] <= data["Q"]*z[i-1,t]) #(12)
+				end
+			end
+			if(!branch)
+				for j in [2:data["n"]+1;]
+					if(i!=j && i>1)
+						@constraint(m,w[i-1,t]-w[j-1,t] >= q[i,t]-Mit_til(data,i-1,t)*(1-x[i,j,t])) #(11)
+					end
+				end
+			end
+		end
+	end
+	
+	function lazySep_Violated(cb_data)
+		for t = 1:data["l"]
+			xsep=zeros((data["n"]+1,data["n"]+1))
+			for i=1:data["n"]+1
+				for j =1:data["n"]+1
+					if( round(callback_value(cb_data,x[i,j,t]))==1)
+						xsep[i,j]=1
+					end
+				end
+			end
+			trouveDepartDepot,sousTours=detect_sous_tour1(data["n"],xsep)
+			if(!trouveDepartDepot) # trouver au moins un sous tours
+				for tournee in sousTours
+					if(length(tournee)>=2)
+						#con = @build_constraint(sum(variable_by_name(m,"x[$i,$j,$t]") for i in tournee for j in tournee if i!=j )*data["Q"] <= sum( data["Q"]*variable_by_name(m,"z[$i,$t]") - variable_by_name(m,"q[$i,$t]") for i in tournee))
+						con = @build_constraint(sum(x[i,j,t] for i in tournee for j in tournee if i!=j )*data["Q"] <= sum( data["Q"]*z[i-1,t] - q[i,t] for i in tournee))
+						MOI.submit(m,MOI.LazyConstraint(cb_data),con)
+						sbar=setdiff(Set(1:data["n"]+1),tournee)
+						con = @build_constraint(sum(x[i,j,t] for i in sbar for j in tournee) >= sum(q[i,t] for i in tournee)/data["Q"]) 
+						MOI.submit(m,MOI.LazyConstraint(cb_data),con)
+			
+					end
+				end
+			end
+		end
+	end
+	set_time_limit_sec(m,600)
+	MOI.set(m,MOI.LazyConstraintCallback(),lazySep_Violated)
+	optimize!(m)
+	
+	status = termination_status(m)
+	if status == JuMP.MathOptInterface.INFEASIBLE
+		println("Le problème n'est pas réalisable")
+	elseif status == JuMP.MathOptInterface.DUAL_INFEASIBLE
+		println("Le problème est non borné")
+	elseif status == JuMP.MathOptInterface.OPTIMAL
+		xopt,yopt,qopt,popt=get_param_opt1(data,m)
+		println("Temps de résolution :", solve_time(m))
+		println("optimum = ", objective_value(m)) 
+		return true,objective_value(m), solve_time(m) ,xopt,yopt,qopt,popt,relative_gap(m)
+		#println(solution_summary(m, verbose=true))
+		#return true,
+	end
+	return false,0,0,0,0,0,0,0
+	
+
+end
+
+
+
---
+++ toolsPDI.jl
@@ -0,0 +1,239 @@
+#---------------------------------------------------------------
+#PRP 1
+#---------------------------------------------------------------
+
+function PDI1_Exact_to_Circuit(data, x) 
+    """
+	Permet de représenter la solution renvoyé par le PL sous forme de liste de liste (représentant les circuits, dans l'ordre de passage)
+    Paramètres : 
+    data le dictionnaire contenant les données de l'instance
+	x les valeurs des variables binaires xij qui vallent 1 si un véhicule utilise l’arc (i, j) 
+	affichage qui vaut true si on veut afficher le graphe
+    """
+
+	# Récupération des données
+	n = data["n"] # n le nombre de clients	
+
+	res_circuits = []
+	for t =1:data["l"]
+		circuits = Vector{Int64}[]
+		for i in 2:n+1
+			# On sait que tout chemin commence par le sommet 0 (centre de dépôt)
+			if x[1,i,t] > 0
+				passeDepot,circuit=find_tournee(n,x[:,:,t],i,1)
+				for i =1 : length(circuit)
+					circuit[i]=circuit[i]-1
+				end
+				push!(circuits, circuit)
+
+			end
+		end
+		push!(res_circuits,circuits)
+	end
+	return res_circuits
+end
+
+
+
+function detect_sous_tour1(n,xkt)
+	"""
+	n: nb de revendeurs
+	xkt : matrice x[:,:,k,t], contient peut etre plusieurs tournees
+	detecter un sous tour d une voiture k, a l instant t
+	"""
+	sousTours=Set()
+	dejaVisite=Set()
+	trouveDepartDepot=true # =false si trouver au moins un sous tour
+	for i =2:n+1
+		if(!(i in dejaVisite) ) #si deja visite, appartient deja a un sous tours
+			for j =2:n+1
+				if(xkt[i,j]>=1)
+					passeDepot,tournee=find_tournee(n,xkt,j,i)
+					if(!passeDepot && !(1 in tournee)) #detection d un sous tours
+						trouveDepartDepot=false
+						push!(sousTours, Set(tournee))
+						union!(dejaVisite,tournee)
+
+					end
+				end
+			end
+		end
+
+	end
+	#print(sousTours)
+	return trouveDepartDepot,sousTours
+end
+
+
+function get_param_opt1(data,m)
+	"""
+	m : model
+	data : donnees de l instance
+	"""
+	xopt=zeros(Int,data["n"]+1,data["n"]+1,data["l"])
+	yopt=zeros(Int,data["l"])
+	qopt=zeros((data["n"]+1,data["l"]))
+	popt=zeros(data["l"])
+	for t =1:data["l"]
+		yopt[t]=value(variable_by_name(m,"y[$t]"))
+		popt[t] = value(variable_by_name(m,"p[$t]"))
+		for i=1:data["n"]+1
+			qopt[i,t]=value(variable_by_name(m,"q[$i,$t]"))
+			for j=1:data["n"]+1
+				xopt[i,j,t]=round(value(variable_by_name(m,"x[$i,$j,$t]")))
+			end
+		end
+	end
+	return xopt,yopt,qopt,popt
+end
+
+
+
+#---------------------------------------------------------------
+#PRP 2
+#---------------------------------------------------------------
+
+
+function PDI2_Exact_to_Circuit(data, x) 
+    """
+	Permet de représenter la solution renvoyé par le PL sous forme de liste de liste (représentant les circuits, dans l'ordre de passage)
+    Paramètres : 
+    data le dictionnaire contenant les données de l'instance
+	x les valeurs des variables binaires xij qui vaut 1 si un véhicule utilise l’arc (i, j) 
+	affichage qui vaut true si on veut afficher le graphe
+    """
+
+	# Récupération des données
+	n = data["n"] # n le nombre de clients	
+
+	res_circuits = [] #doit avoir data["l"] liste de circuits, dont chaque circuit contient <=k listes
+	for t =1:data["l"]
+		circuits = []
+		for k =1:data["k"]
+			#une voiture est utilisee
+			if(sum(x[:,:,k,t])>1) #car x[:,1,k,t]+[1,:,k,t]==2
+				#trouver le premier revendeur a visiter
+				
+				for i=2:n+1
+					if(x[1,i,k,t]>0)
+						passeDepot,circuit=find_tournee(n,x[:,:,k,t],i,1)
+						push!(circuits,circuit)
+						break
+					end
+				end
+			end
+		end
+		push!(res_circuits,circuits)
+	end
+	return res_circuits
+end
+
+
+
+
+function detect_sous_tour2(n,xkt)
+	"""
+	n: nb de revendeurs
+	xkt : matrice x[:,:,k,t]
+	detecter un sous tour d une voiture k, a l instant t
+	xkt contient une tournee de la voiture k
+	"""
+	for i =2:n+1
+		for j =1:n+1
+			if(xkt[i,j]>0)
+				return find_tournee(n,xkt,j,i)
+			end
+		end
+
+	end
+
+end
+
+function get_param_opt2(data,m)
+	"""
+	m : model
+	data : donnees de l instance
+	"""
+	xopt=zeros(Int,data["n"]+1,data["n"]+1,data["k"],data["l"])
+	yopt=zeros(Int,data["l"])
+	qopt=zeros((data["n"]+1,data["k"],data["l"]))
+	popt=zeros(Int,data["l"])
+	for t =1:data["l"]
+		yopt[t]=value(variable_by_name(m,"y[$t]"))
+		popt[t]=value(variable_by_name(m,"p[$t]"))
+		for k =1:data["k"]
+			for i=1:data["n"]+1
+				qopt[i,k,t]=value(variable_by_name(m,"q[$i,$k,$t]"))
+				for j=1:data["n"]+1
+					xopt[i,j,k,t]=value(variable_by_name(m,"x[$i,$j,$k,$t]"))
+				end
+			end
+		end
+	end
+	return xopt,yopt,qopt,popt
+end
+
+#---------------------------------------------------------------
+#COMMUN SOUS TOURS, BigM
+#---------------------------------------------------------------
+
+function find_tournee(n,xkt,i,circuit)
+	"""
+	circuit: un chiffre, le point de depart du circuit
+	n : nb de revendeur
+	i : le point de depart suivant
+	xkt : matrice x de la voiture k, a l instant t
+	donne le circuit de la voirture k a l instant t
+	"""
+	depart=i
+	tournee=[circuit]
+	findDepot=false
+	dejaPasse=Set() #stock les noeuds deja passes
+	push!(dejaPasse,circuit)
+	#println(xkt)
+	while(!findDepot && !(depart in dejaPasse))
+		push!(tournee,depart)
+		push!(dejaPasse,depart)
+		for j =1:n+1
+			if(xkt[depart,j]>0)
+				depart=j
+				break
+			end
+		end
+		if(depart==1)
+			if(!(1 in tournee))
+				push!(tournee,depart)
+			end
+			findDepot=true
+		end
+	end
+	#println(tournee)
+	return findDepot,tournee
+end
+
+
+function Mt(data,t)
+	s=0
+	for T in [t : data["l"];]
+		for i in [1 : data["n"];]
+			s=s+data["d"][i,T]
+		end
+	end
+	return min(data["C"],s)
+end
+
+function Mit_til(data,i,t)
+	s=0
+	for j in [t : data["l"];]
+		s=s+data["d"][i,j]
+	end
+	return min(data["L"][i],data["Q"],s)
+end
+
+
+
+
+
+
+
+
